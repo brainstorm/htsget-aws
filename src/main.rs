939e1e7e1d8b45e22a31d4b56e67ee99ab202524
brainstorm <brainstorm@nopcode.org>
2019-07-09 12:13:11 +1000

    Basic search/index CLI scaffolding

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,17 @@ extern crate clap;
 
 use clap::{App, Arg, SubCommand};
 
+fn htsget_index(location: String) {
+    println!("Indexing file: {}", location)
+}
+
+fn htsget_search(id: String) {
+    println!("Let's search: {}", id)
+}
+
 fn main() {
-    let _matches = App::new(crate_name!())
+    // CLI definition...
+    let matches = App::new(crate_name!())
                         .version(crate_version!())
                         .author(crate_authors!())
                         .about("Retrieve bioinformatics data using REST")
@@ -19,4 +28,16 @@ fn main() {
                                     .help("Bioinformatic attribute ID, i.e: chr1")
                                     .required(true)))
                         .get_matches();
+
+    // ... and some argument action!
+    match matches.subcommand() {
+        ("index", Some(index_matches)) => {
+            htsget_index(index_matches.value_of("location").unwrap().to_string());
+        },
+        ("search", Some(search_matches)) => {
+            htsget_search(search_matches.value_of("id").unwrap().to_string());
+        },
+        ("", None)   => println!("{}", matches.usage()),
+        _            => unreachable!(), // If all subcommands are defined above, anything else is unreachable!()
+    }
 }
