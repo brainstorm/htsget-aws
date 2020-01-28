2547be6488a1f730b9e302338b561b7481de289c
Roman Valls Guimera <brainstorm@nopcode.org>
2020-01-28 13:54:05 +1100

    Loads all vector of bytes into memory since Nom streaming is not pause/resume-able: https://github.com/brainstorm/bio-index-formats/commit/8272a1c6c6a590f0cb16b66113c33308cee2bb29. Next up, use async-await lambda-runtime branch as suggested here: https://github.com/awslabs/aws-lambda-rust-runtime/issues/14#issuecomment-569046122

--- reads/src/main.rs
+++ reads/src/main.rs
@@ -3,9 +3,15 @@ use lambda_runtime::{error::HandlerError, Context};
 
 use reads::{Format, Class, htsget_response, htsget_request, reference_ids, bucket_obj_bytes};
 use bio_index_formats::parser_bai::parse_bai;
+
+use futures::stream::StreamExt;
 use rusoto_core::Region;
 use rusoto_s3::S3Client;
+use serde_json::json;
 
+// https://github.com/awslabs/aws-lambda-rust-runtime/issues/14#issuecomment-569046122
+//#[tokio::main]
+//async fn main() {
 fn main() {
     // Init env logger for debugging: https://www.rusoto.org/debugging.html
     let _ = env_logger::try_init();
@@ -13,6 +19,7 @@ fn main() {
 }
 
 async fn handler(
+//fn handler(
     _req: Request,
     _ctx: Context,
 ) -> Result<impl IntoResponse, HandlerError> {
@@ -29,14 +36,27 @@ async fn handler(
     let auth = "Bearer: foo".to_string();
 
     // Get BAI from AWS
-    let bai_bytes = bucket_obj_bytes(s3, bucket, obj_bai_path).await.unwrap();
+    let bai_stream = bucket_obj_bytes(s3, bucket, obj_bai_path).await.unwrap();
 
 
     // https://stackoverflow.com/questions/57810173/streamed-upload-to-s3-with-rusoto/59884256#comment102487432_57812269
     // let byte_stream = FramedRead::new(bai_bytes, BytesCodec::new()).map(|r| r.freeze())?;
 
+    /*
+    Sadly it's become a bit painful to concatenate a stream of Bytes at the moment
+    (see: https://github.com/tokio-rs/bytes/issues/324)
+    In the meantime doing a while let Some(bytes) = stream.next().await { ... }
+    and extend some Vec<u8> inside the loop then pass that to parse_bai
+    */
+
+    let mut bai_iter = bai_stream;
+    let mut bai_vec = Vec::<u8>::new();
+    while let Some(b) = bai_iter.next().await {
+        bai_vec.push(b.unwrap()[0]);
+    }
+
     // Parse BAI
-    let bai = parse_bai(bai_bytes);
+    let bai = parse_bai(&bai_vec);
     let refs = bai.map(|r| r.1.refs).unwrap();
 
     // Get "symbols" such as chr1 from BAM header
@@ -48,7 +68,5 @@ async fn handler(
     let range = htsget_request(reference, chrom_start, chrom_end);
     let res = htsget_response(auth, range, bucket, Format::BAM, Class::Body);
 
-    //let htsget = serde_json::to_string(&container)?;
-    Ok(serde_json::to_string(&res)?)
-    //Ok(json!(htsget))
+    Ok(json!(res))
 }
