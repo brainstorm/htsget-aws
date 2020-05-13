c3586d0280ec33a2531cb206b44bbbdea73e0c31
Roman Valls Guimera <brainstorm@nopcode.org>
2020-05-13 18:27:09 +1000

    Tell cargo that libz-sys is statically handled on reads, experiment with the rest of remaining linker flags from rustc

---
+++ reads/build.rs
@@ -0,0 +1,10 @@
+
+fn main() {
+    let mut cfg = cc::Build::new();
+    cfg.warnings(true).static_flag(true).pic(true);
+    println!("cargo:rustc-link-lib=static=z");
+    //println!("cargo:rustc-link-lib=static=dl");
+    //println!("cargo:rustc-link-lib=static=util");
+    //println!("cargo:rustc-link-lib=compiler_rt");
+    //println!("cargo:rustc-link-lib=static=pthread");
+}
