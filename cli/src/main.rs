8ece644a309f9b6ed03da10e06dbd3e11e8c7cc7
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2020-10-15 23:47:23 +0000

    Add scaffolding for http mock testing. Move away from GitHub codespaces Beta since the lower 2-CPU tier does not compile this fast enough to be practical

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -29,3 +29,29 @@ fn main() {
         _            => unreachable!(), // If all subcommands are defined above, anything else is unreachable!()
     }
 }
+
+use httpmock::Method::{GET};
+use httpmock::{Mock, MockServer};
+
+#[test]
+fn example_test() {
+    // Start a local mock server for exclusive use by this test function.
+    let mock_server = MockServer::start();
+
+    // Create a mock on the mock server. The mock will return HTTP status code 200 whenever
+    // the mock server receives a GET-request with path "/hello".
+    let search_mock = Mock::new()
+        .expect_method(GET)
+        .expect_path("/hello")
+        .return_status(200)
+        .create_on(&mock_server);
+
+    // Send an HTTP request to the mock server. This simulates your code.
+    // The mock_server variable is being used to generate a mock server URL for path "/hello".
+    let response = reqwest::get(&mock_server.url("/hello")).await?;
+
+    // Ensure the mock server did respond as specified above.
+    assert_eq!(response.status(), 200);
+    // Ensure the specified mock responded exactly one time.
+    assert_eq!(search_mock.times_called(), 1);
+}
